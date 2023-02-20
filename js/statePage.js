72395edec7a4ea2abd9c74ba3ad8f7dd524ffb8c
David Rincon Toro <leon55490@gmail.com>
2023-02-19 20:55:37 -0500

    Mejora de diseño

--- js/statePage.js
+++ js/statePage.js
@@ -1,61 +1,53 @@
 function encender() {
-    if (Number(memoriaInput.value) > 9999) {
-        alert('EL ESPACIO DE MEMORIA ES INSUFICIENTE');
-        apagar();
-    }
-    kernel.disabled = true;
-    memoriaInput.disabled = true;
-    inputMetodoAlgoritm.disabled = true;
-    btnEncender.style.display = 'none';
-    btnApagar.style.display = 'inline-block';
+	if (Number(memoriaInput.value) > 9999) {
+		alert('EL ESPACIO DE MEMORIA ES INSUFICIENTE');
+		apagar();
+	}
+	kernel.disabled = true;
+	memoriaInput.disabled = true;
+	inputMetodoAlgoritm.disabled = true;
+	btnEncender.style.display = 'none';
+	btnApagar.style.display = 'inline-block';
 
-    modo.innerHTML = 'Modo usuario';
-    cerrar.style.display = 'block';
-    files.disabled = false;
+	modo.innerHTML = 'Modo usuario';
+	cerrar.style.display = 'block';
+	files.disabled = false;
 
-    // ARRAY DE SISTEMA OPERATIVO Y KERNEL CON LIMITE DE MEMORIA
-    let memoriaMostrar = [];
-    initialPosition += Number(kernel.value) + 1
+	// ARRAY DE SISTEMA OPERATIVO Y KERNEL CON LIMITE DE MEMORIA
+	let memoriaMostrar = [];
+	initialPosition += Number(kernel.value) + 1;
 
+	let lAcumulador = [0, ' Acumulador'];
+	memoriaMostrar.push(lAcumulador.toString().replaceAll(',', ' '));
+	// console.log(memoriaMostrar);
+	for (let i = 1; i < Number(memoriaInput.value) + 1; i++) {
+		if (i <= Number(kernel.value)) {
+			let so = [i];
 
-    let lAcumulador = [0, ' Acumulador']
-    memoriaMostrar.push(lAcumulador.toString().replaceAll(',', ' '));
-    // console.log(memoriaMostrar);
-    for (let i = 1; i < Number(memoriaInput.value) + 1; i++) {
-        if (i <= Number(kernel.value)) {
-            let so = [i];
+			so.push('Ocupado Por El Sistema');
+			memoriaMostrar.push(so.toString().replaceAll(',', ' '));
+		} else {
+			memoriaMostrar.push(`${i} - - - - `);
+		}
+	}
 
-            so.push('USE FOR SYSTEM');
-            memoriaMostrar.push(so.toString().replaceAll(',', ' '));
-        } else {
-            memoriaMostrar.push(`${i} - - - - `)
-        }
-    }
+	if (inputMetodoAlgoritm.value === 'RR' && quantum === 0) {
+		let quantum = prompt(`Ingrese el valor del quantum, ya que en defecto será (5)`);
+		quantum = quantum ? Number(quantum) : 5;
+		inputQuantum.value = quantum;
+	}
 
-    if (inputMetodoAlgoritm.value === 'RR' && quantum === 0) {
-        let quantum = prompt(`Ingrese el valor del quantum, ya que en defecto será (5)`);
-        quantum = quantum ? Number(quantum) : 5;
-        inputQuantum.value = quantum;
-    }
-
-
-
-
-
-
-    document.getElementById('memoria').style.display = 'block';
-    document.getElementById('memoria').innerHTML = memoriaMostrar.join('<br></br>');
-
+	document.getElementById('memoria').style.display = 'block';
+	document.getElementById('memoria').innerHTML = memoriaMostrar.join('<br></br>');
 }
 
-
 function apagar() {
-    location.reload()
-    kernel.disabled = false;
-    memoriaInput.disabled = false;
-    inputMetodoAlgoritm.disabled = false;
-    btnApagar.style.display = 'none';
-    btnEncender.style.display = 'inline-block';
-    modo.innerHTML = 'Modo kernel';
-    files.disabled = true;
+	location.reload();
+	kernel.disabled = false;
+	memoriaInput.disabled = false;
+	inputMetodoAlgoritm.disabled = false;
+	btnApagar.style.display = 'none';
+	btnEncender.style.display = 'inline-block';
+	modo.innerHTML = 'Modo kernel';
+	files.disabled = true;
 }
